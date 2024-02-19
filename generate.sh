28ab968e86451a0b5051546d4c63693d63bb789e
Shin Umeda <umeda.shin@gmail.com>
2024-02-19 04:00:59 -0800

    fixing line endings on shells scripts

--- generate.sh
+++ generate.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
+set -e
 eval $(git for-each-ref --shell \
   --format='git checkout %(objectname) && poetry run mike deploy %(refname:lstrip=2);' \
   refs/heads/)
+poetry run mike set-default main
 git checkout gh-pages
